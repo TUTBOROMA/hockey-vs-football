4e6aea4d6cca88b407d3660feb2ff904371e6453
Roman <tutboroma7777@gmail.com>
2025-03-13 20:53:34 +0300

    Initial commit: готовый сайт Хоккей vs Футбол

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
+from flask import Flask, render_template
 from datetime import datetime
+
+app = Flask(__name__)
 
 @app.context_processor
 def inject_now():
     return {'now': datetime.utcnow()}
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
