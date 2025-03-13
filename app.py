2e9bb564470c320d177c7f95235724a233307445
Roman <tutboroma7777@gmail.com>
2025-03-13 20:48:28 +0300

    Initial commit: готовый сайт Хоккей vs Футбол

--- app.py
+++ app.py
@@ -1,22 +1,5 @@
-from flask import Flask, render_template
+from datetime import datetime
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/hockey")
-def hockey():
-    return render_template("hockey.html")
-
-@app.route("/football")
-def football():
-    return render_template("football.html")
-
-@app.route("/compare")
-def compare():
-    return render_template("compare.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.context_processor
+def inject_now():
+    return {'now': datetime.utcnow()}
